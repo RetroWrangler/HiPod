6ca4c636f226eea24fb9c9db4e431196ad730aa5
Cory Dowdy <csaw700@icloud.com>
2025-10-22 10:44:55 -0400

    Initial Commit

---
+++ HiPod/HiPodApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiPodApp.swift
+//  HiPod
+//
+//  Created by Cory on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct HiPodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
